d80a9832cb526710d1468413b039554fedbd4c42
Oihana Mendez <oihanamv92@gmail.com>
2021-10-21 01:25:12 +0200

    first commit

---
+++ src/es/Studium/Recorre/Recorre.java
@@ -0,0 +1,26 @@
+package es.Studium.Recorre;
+
+public class Recorre
+{
+
+	public static void main(String[] args)
+	{
+		String cadena = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+		for (int i = 0; i<cadena.length();i++)
+		{
+			System.out.printl(cadena.charAt(i));
+		}
+
+	}
+
+}
+
+/* Programa Recorre
+ * VARIABLES
+ * CADENA cadena = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+ * ENTERO i
+ * INICIO
+ * PARA i = 0 HASTA i <lONGITUD (cadena) HACER
+ * 	ESCRIBIR POSICION (cadena,i)
+ * FIN PARA
+ *FIN */
